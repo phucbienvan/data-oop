c3354b9b59204dbddd538dffa986003fafb00aa4
phucbienvan <78720117+phucbienvan@users.noreply.github.com>
2021-04-02 20:40:35 +0700

    Update business.php

--- data/business.php
+++ data/business.php
@@ -22,8 +22,8 @@ class business extends database{
         return $this->update($this->_table_name, $data, $this->_key."=".(int)$id);
     }
  
-    function select_by_id($select, $id){
-        $sql = "select $select from ".$this->_table_name." where ".$this->_key." = ".(int)$id;
+    function select_by_id($id){
+        $sql = "select from ".$this->_table_name." where ".$this->_key." = ".(int)$id;
         return $this->get_row($sql);
     }
 }
